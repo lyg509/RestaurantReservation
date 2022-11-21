6a30d95e956a46227616fa0d0415aae3b9c137bc
lyg509 <dydrjs252@naver.com>
2022-11-22 08:19:03 +0900

    가게 상세 기능 추가

--- src/main/java/com/lyg/eatgo/domain/Restaurant.java
+++ src/main/java/com/lyg/eatgo/domain/Restaurant.java
@@ -2,23 +2,29 @@ package com.lyg.eatgo.domain;
 
 public class Restaurant {
 
+    private final Long id;
     private final String address;
     private final String name;
 
-    public Restaurant(String name, String address) {
+    public Restaurant(Long id,String name, String address) {
+        this.id = id;
         this.name = name;
         this.address = address;
     }
 
-    public String getName() {
-        return  "Bob zip";
+    public Long getId() {
+        return id;
     }
 
-    public String getInformation() {
-        return name + " in " + address;
+    public String getName() {
+        return  name;
     }
 
     public String getAddress() {
         return address;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
     }
 }
--- src/test/java/com/lyg/eatgo/interfaces/RestaurantControllerTest.java
+++ src/test/java/com/lyg/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,4 +1,56 @@
-import static org.junit.Assert.*;
+package com.lyg.eatgo.interfaces;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)    //스프링을 이용해서 테스트
+@WebMvcTest(RestaurantController.class) //특정 controller를 테스트
 public class RestaurantControllerTest {
-  
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Bob zip\"")
+                ));
+    }
+
+    //가게 상세
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                                containsString("\"name\":\"Bob zip\"")
+                ));
+
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":2020")
+                ))
+                .andExpect(content().string(
+                                containsString("\"name\":\"Cyber Food\"")
+                ));
+    }
 }
