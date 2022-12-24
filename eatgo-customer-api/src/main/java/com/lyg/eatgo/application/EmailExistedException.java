f5932334c5537304430da0a921e9361f2483d140
lyg509 <dydrjs252@naver.com>
2022-12-24 18:40:05 +0900

    회원 가입

---
+++ eatgo-customer-api/src/main/java/com/lyg/eatgo/application/EmailExistedException.java
@@ -0,0 +1,8 @@
+package com.lyg.eatgo.application;
+
+public class EmailExistedException extends  RuntimeException{
+
+    EmailExistedException(String email){
+        super("Email is already registered: "+ email);
+    }
+}
---
+++ eatgo-customer-api/src/test/java/com/lyg/eatgo/application/UserServiceTest.java
@@ -0,0 +1,49 @@
+package com.lyg.eatgo.application;
+
+import com.lyg.eatgo.domain.User;
+import com.lyg.eatgo.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+class UserServiceTest {
+
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    public void registerUserWithExistedEmail() {
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        User user = User.builder().build();
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+
+
+        assertThatThrownBy(() -> {
+            userService.registerUser(email, name, password);
+        }).isInstanceOf(EmailExistedException.class);
+
+        verify(userRepository, never()).save(any());
+    }
+
+}
